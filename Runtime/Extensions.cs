437fdf17f9587d16e606f0ba3b0e0cd7a5c730a7
Hunter MacDonald <mactinite77@gmail.com>
2021-05-17 16:47:24 -0700

    Add an extension method to convert force to torque

---
+++ Runtime/Extensions.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+namespace mactinite.ToolboxCommons
+{
+    public static class Extensions
+    {
+        public static float ForceToTorque(this Rigidbody2D rigidbody2D, Vector2 force, Vector2 position, ForceMode2D forceMode = ForceMode2D.Force)
+        {
+            // Vector from the force position to the CoM
+            Vector2 p = rigidbody2D.worldCenterOfMass - position;
+
+            // Get the angle between the force and the vector from position to CoM
+            float angle = Mathf.Atan2(p.y, p.x) - Mathf.Atan2(force.y, force.x);
+
+            // This is basically like Vector3.Cross, but in 2D, hence giving just a scalar value instead of a Vector3
+            float t = p.magnitude * force.magnitude * Mathf.Sin(angle) * Mathf.Rad2Deg;
+
+            // Continuous force
+            if (forceMode == ForceMode2D.Force) t *= Time.fixedDeltaTime;
+
+            // Apply inertia
+            return t / rigidbody2D.inertia;
+        }
+    }
+    
+}
