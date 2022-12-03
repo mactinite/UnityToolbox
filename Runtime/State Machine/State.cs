f0c82125c0cb34ee70053ed60042ee621c299bb6
Hunter MacDonald <mactinite77@gmail.com>
2022-12-03 00:51:47 -0800

    Update state manager, now uses a dictionary to cache state instances

--- Runtime/State Machine/State.cs
+++ Runtime/State Machine/State.cs
@@ -1,38 +1,28 @@
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-namespace mactinite.ToolboxCommons.StateMachine
-{
-    public abstract class State<T> where T : StateMachine<T>
-    {
+namespace mactinite.ToolboxCommons.StateMachine {
+    public abstract class State<T> where T : StateMachine<T> {
         private T stateMachine;
 
-        public T StateMachine
-        {
-            get => stateMachine;
-            set => stateMachine = value;
-        }
+        public T StateMachine { get => stateMachine; set => stateMachine = value; }
 
-        public State(T stateMachine)
-        {
+        public State (T stateMachine) {
             StateMachine = stateMachine;
         }
 
-        public State()
-        {
-        }
+        public State () { }
 
-        public virtual IEnumerator Start()
-        {
+        public virtual IEnumerator Start () {
             yield break;
         }
 
-        public virtual IEnumerator Tick()
-        {
+        public virtual IEnumerator Tick () {
             yield break;
         }
 
-        public virtual IEnumerator End()
-        {
+        public virtual IEnumerator End () {
             yield break;
         }
     }
--- Runtime/State Machine/StateMachine.cs
+++ Runtime/State Machine/StateMachine.cs
@@ -2,56 +2,41 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace mactinite.ToolboxCommons.StateMachine
-{
+namespace mactinite.ToolboxCommons.StateMachine {
 
-    public abstract class StateMachine<T> : MonoBehaviour where T : StateMachine<T>
-    {
-        protected State<T> State;
-        public Dictionary<string, State<T>> StateMap = new Dictionary<string, State<T>>();
+    public abstract class StateMachine<T> : MonoBehaviour where T : StateMachine<T> {
 
-        public void SetState(State<T> state)
-        {
+        private State<T> State = null;
+        public Dictionary<string, State<T>> StateMap = new Dictionary<string, State<T>> ();
+
+        private IEnumerator SetState (State<T> state) {
             if (State != null)
-                StartCoroutine(State.End());
+                yield return StartCoroutine (State.End ());
             State = state;
-            StartCoroutine(State.Start());
+            yield return StartCoroutine (State.Start ());
         }
 
-        public void SetState<S>() where S : State<T>, new()
-        {
-            State<T> stateInstance = new S();
-            stateInstance.StateMachine = (T) this;
-            if (State != null)
-                StartCoroutine(State.End());
-            State = stateInstance;
-            StartCoroutine(State.Start());
-        }
-
-        public void SetState(string stateName)
-        {
-            if (StateMap.TryGetValue(stateName, out State))
-            {
-                SetState(State);
-            }
-            else
-            {
-                throw new Exception(stateName +
-                                    " not found in runtime state map. Did you forget to register the state or use the wrong identifier?");
+        public void SetState (string stateName) {
+            if (StateMap.TryGetValue (stateName, out var nextState)) {
+                StartCoroutine (SetState (nextState));
+            } else {
+                throw new Exception (
+                    $"{stateName} not found in runtime state map. \n" +
+                    $"Did you forget to register the state or use the wrong identifier?");
             }
         }
 
-
-        public void Update()
-        {
+        public void Update () {
             if (State != null)
-                StartCoroutine(State.Tick());
+                StartCoroutine (State.Tick ());
         }
 
-        private void OnDestroy()
-        {
-            this.StopAllCoroutines();
+        private void OnDestroy () {
+            this.StopAllCoroutines ();
+        }
+
+        private void Awake () {
+            State = null;
         }
     }
 }
-
