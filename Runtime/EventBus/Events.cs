01fcc0ab86a20e2480981b64932ecea3473071b0
mactinite <mactinite@users.noreply.github.com>
2025-12-20 04:03:03 -0800

    Add an event bus

---
+++ Runtime/EventBus/Events.cs
@@ -0,0 +1,12 @@
+﻿namespace toolbox.EventBus
+{
+    public interface IEvent { }
+    
+    // Examples:
+    // public struct TestEvent : IEvent { }
+    //
+    // public struct PlayerEvent : IEvent {
+    //     public int health;
+    //     public int mana;
+    // }
+}
